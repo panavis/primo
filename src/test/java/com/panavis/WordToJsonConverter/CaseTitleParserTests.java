84328fcb58d26cf15fe2f0b89c6013f8b508dba6
Anselme Mucunguzi <mucunguzia@gmail.com>
2019-03-07 20:19:34 -0600

    unbundled tests for the title sections and wrote a separate unit test for each case

--- src/test/java/com/panavis/WordToJsonConverter/CaseTitleParserTests.java
+++ src/test/java/com/panavis/WordToJsonConverter/CaseTitleParserTests.java
@@ -3,20 +3,110 @@ package com.panavis.WordToJsonConverter;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 public class CaseTitleParserTests {
 
     @BeforeClass
     public static void setUp() {
         TestsSetup.setUp();
+        CaseTitleTestsHelpers.setupActualAndExpectedCaseTitles();
     }
 
     @Test
-    public void getCaseTitleShouldReturnTheTitleOfACaseFromItsWordDocument() {
-        ArrayList<String> actualCaseTitles = CaseTitleParserTestsHelpers.getActualCaseTitles(TestsSetup.wordDocxData);
-        ArrayList<String> expectedCaseTitles = CaseTitleParserTestsHelpers.getExpectedCaseTitles(TestsSetup.expectedJsonContent);
-        CaseTitleParserTestsHelpers.compareExpectedToActualTitles(actualCaseTitles, expectedCaseTitles);
+    public void comm_court_huye_2011_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(0);
     }
 
+    @Test
+    public void comm_court_huye_2016_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(1);
+    }
+
+    @Test
+    public void comm_court_huye_2018_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(2);
+    }
+
+    @Test
+    public void comm_court_musanze_2011_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(3);
+    }
+
+    @Test
+    public void comm_court_nyarugenge_2014_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(4);
+    }
+
+    @Test
+    public void comm_court_nyarugenge_2016_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(5);
+    }
+
+    @Test
+    public void comm_high_court_2016_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(6);
+    }
+
+    @Test
+    public void high_court_chamber_nyanza_2014_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(7);
+    }
+
+    @Test
+    public void high_court_chamber_nyanza_2018_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(8);
+    }
+
+    @Test
+    public void high_court_criminal_2011_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(9);
+    }
+
+    @Test
+    public void interm_court_huye_2008_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(10);
+    }
+
+    @Test
+    public void interm_court_huye_2015_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(11);
+    }
+
+    @Test
+    public void interm_court_huye_2016_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(12);
+    }
+
+    @Test
+    public void interm_court_huye_2018_226_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(13);
+    }
+    @Test
+    public void interm_court_huye_2018_aff_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(14);
+    }
+
+    @Test
+    public void primary_court_nzige_2011_rp0003_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(15);
+    }
+
+    @Test
+    public void primary_court_nzige_2011_rp0020_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(16);
+    }
+
+    @Test
+    public void supreme_court_comm_2009_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(17);
+    }
+
+    @Test
+    public void supreme_court_comm_2017_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(18);
+    }
+
+    @Test
+    public void supreme_court_criminal_2018_caseHasExpectedTitle() {
+        CaseTitleTestsHelpers.assertActualTitleMatchesExactlyExpectedTitle(19);
+    }
 }
---
+++ src/test/java/com/panavis/WordToJsonConverter/CaseTitleTestsHelpers.java
@@ -0,0 +1,48 @@
+package com.panavis.WordToJsonConverter;
+
+import com.panavis.WordToJsonConverter.Constants.Keywords;
+import com.panavis.WordToJsonConverter.ResultTypes.SectionResult;
+import com.panavis.WordToJsonConverter.Wrappers.JsonArray;
+import com.panavis.WordToJsonConverter.Wrappers.JsonObject;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+class CaseTitleTestsHelpers {
+
+    private static List<String> allActualCaseTitles = new ArrayList<>();
+    private static List<String> allExpectedCaseTitles = new ArrayList<>();
+
+    static void setupActualAndExpectedCaseTitles() {
+        setupActualCaseTitles();
+        setUpExpectedCaseTitles();
+    }
+
+    private static void setupActualCaseTitles() {
+        for (XWPFDocument wordDoc : TestsSetup.wordDocxData) {
+            Converter converter = TestsSetup.getConverterObject(wordDoc, Keywords.TITLE);
+            converter.parseCaseSections();
+            SectionResult titleResult = converter.getParsedCaseSection(Keywords.TITLE);
+            JsonObject titleObject = titleResult.getSectionContent();
+            String jsonTitle = titleObject.getStringByKey(Keywords.TITLE);
+            allActualCaseTitles.add(jsonTitle);
+        }
+    }
+
+    private static void setUpExpectedCaseTitles() {
+        for (JsonObject caseFile : TestsSetup.expectedJsonContent) {
+            JsonArray caseContent =  caseFile.getArrayByKey(Keywords.CASE);
+            JsonObject titleObject = caseContent.getJsonObjectByIndex(0);
+            allExpectedCaseTitles.add(titleObject.getStringByKey(Keywords.TITLE));
+        }
+    }
+
+    static void assertActualTitleMatchesExactlyExpectedTitle(int docIndex) {
+        String actualTitle = allActualCaseTitles.get(docIndex);
+        String expectedTitle = allExpectedCaseTitles.get(docIndex);
+        assertEquals(expectedTitle, actualTitle);
+    }
+}
